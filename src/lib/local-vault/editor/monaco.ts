511ffce5c335dc33970c439740a5d15726fbb94f
wooningeire <22846982+wooningeire@users.noreply.github.com>
2026-09-20 23:41:52 -0700

    fix: use basic mongo import

--- src/lib/local-vault/editor/monaco.ts
+++ src/lib/local-vault/editor/monaco.ts
@@ -1,25 +1,8 @@
-import type { editor as MonacoEditorNamespace } from 'monaco-editor/esm/vs/editor/editor.api.js';
+import * as monaco from 'monaco-editor';
 
-export type MonacoApi = typeof import('monaco-editor/esm/vs/editor/editor.api.js');
-export type MonacoEditor = MonacoEditorNamespace.IStandaloneCodeEditor;
+export type MonacoApi = typeof monaco;
+export type MonacoEditor = monaco.editor.IStandaloneCodeEditor;
 
-let monacoApi: MonacoApi | null = null;
-
-export async function loadMonaco() {
-	if (monacoApi) {
-		return monacoApi;
-	}
-
-	const [monaco] = await Promise.all([
-		import('monaco-editor/esm/vs/editor/editor.api.js'),
-		import('monaco-editor/esm/vs/basic-languages/css/css.contribution.js'),
-		import('monaco-editor/esm/vs/basic-languages/html/html.contribution.js'),
-		import('monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution.js'),
-		import('monaco-editor/esm/vs/basic-languages/markdown/markdown.contribution.js'),
-		import('monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution.js'),
-		import('monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution.js')
-	]);
-	monacoApi = monaco;
-
-	return monacoApi;
+export async function loadMonaco(): Promise<MonacoApi> {
+	return monaco;
 }
